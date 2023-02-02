e7b72d5558a0590bdff2f3d71c732120bda521cd
mchonlatee <chonlatee@nakai.ac.th>
2023-02-02 18:42:48 +0700

    index time

--- assets/js/DG---BS5-Countdown.js
+++ assets/js/DG---BS5-Countdown.js
@@ -8,7 +8,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "02/10/",
+      dayMonth = "02/16/",
       Endtime = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
